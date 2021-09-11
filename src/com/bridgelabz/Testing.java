c7f15d271d59a7e1d62a9b028c36751a5430a5c5
Dinesh Kumar <mr.dineshdkd@gmail.com>
2021-09-11 16:15:48 +0530

    WLCOME

--- src/com/bridgelabz/Testing.java
+++ src/com/bridgelabz/Testing.java
@@ -4,5 +4,6 @@ public class Testing {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Dinesh");
+        System.out.println("New Delhi");
     }
 }
